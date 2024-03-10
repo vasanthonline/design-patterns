5e50cf28cf1bd99f9da129e216c368ab26486094
Vasanth Gopal <vasanth.gopal0@loblaw.ca>
2024-03-10 15:04:42 -0400

    Create design pattern for factory method

---
+++ src/main/kotlin/com/designpatterns/templates/TemplatesApplication.kt
@@ -0,0 +1,11 @@
+package com.designpatterns.templates
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemplatesApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemplatesApplication>(*args)
+}
